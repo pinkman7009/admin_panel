a1b95a9d51f1505873f5317eed239f842f459a76
Vivek Matalia <vivekonfire@pop-os.localdomain>
2021-11-08 02:08:52 +0530

    login signup ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
-import {Routes, BrowserRouter,Route} from "react-router-dom"
-import Login from './pages/auth/Login';
-import Signup from './pages/auth/Signup';
-import Mainscreen from "./pages/dashboard/Mainscreen"
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import Mainscreen from "./pages/dashboard/Mainscreen";
 
 const AuthRoutes = () => {
-  return (
-         <>
-           <Routes>
-             <Route exact path='/login' element={Login} />
-             <Route exact path='/signup' element={Signup} />       
-           </Routes>
-         </>
+    return (
+        <>
+            <Routes>
+                <Route exact path="/login" element={<Login />} />
+                <Route exact path="/signup" element={<Signup />} />
+            </Routes>
+        </>
     );
-}
-function App() {
+};
 
-  const login = true;
-  return (
-    <BrowserRouter>
-        {login ? <Mainscreen/> : <AuthRoutes/>}
-    </BrowserRouter>
-  );
+function App() {
+    const login = false;
+    return (
+        <BrowserRouter>{login ? <Mainscreen /> : <AuthRoutes />}</BrowserRouter>
+    );
 }
 
 export default App;
---
+++ frontend/src/components/authcomponents/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../styles/AuthNavbar.css";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div>Admin Panel</div>
+            <div>
+                <ul>
+                    <Link to="/login" style={{ textDecoration: "none" }}>
+                        <li>Login</li>
+                    </Link>
+                    <Link to="/signup" style={{ textDecoration: "none" }}>
+                        <li>Sign Up</li>
+                    </Link>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
