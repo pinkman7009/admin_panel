1439aae3ddc6892aac71ceecb4850cd331d4b69e
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-11-11 03:00:30 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -23,11 +23,11 @@ app.use("/api/plans", require("./backend/routes/plan"));
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-    );
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 }
 
 app.listen(PORT, () => console.log(`Server running at ${PORT}`));
