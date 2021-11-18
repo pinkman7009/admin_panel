bc970717a4b47a272437e8948d0572d5ad9e03b1
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-11-18 11:18:26 +0530

    only some minor ui changes

--- frontend/src/components/ui/customerdetails/CustomerDetailsSection.js
+++ frontend/src/components/ui/customerdetails/CustomerDetailsSection.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "../../../styles/CustomerDetails.css";
 import CustomerDetailsList from "./CustomerDetailsList";
-
+import { FaUser } from "react-icons/fa";
+import { BiTime } from "react-icons/bi";
 const CustomerDetailsSection = () => {
   const customerDetails = [
     {
@@ -44,16 +45,34 @@ const CustomerDetailsSection = () => {
     <div className="cd-container">
       <div className="cd-stats">
         <div className="cd-stats-item">
-          <h3>Total Users</h3>
-          <p>3400</p>
+          <div>
+            <h3>Total Users</h3>
+            <p>3400</p>
+          </div>
+          <div className="cd-stats-item-icon">
+            <FaUser />
+          </div>
         </div>
         <div className="cd-stats-item">
-          <h3>New Users</h3>
-          <p>500</p>
+          <div>
+            <h3>New Users</h3>
+            <p>500</p>
+          </div>
+          <div className="cd-stats-item-icon">
+            <img
+              src="https://cdn.iconscout.com/icon/free/png-256/bar-graph-2119073-1790556.png"
+              alt=""
+            />
+          </div>
         </div>
         <div className="cd-stats-item">
-          <h3>Active Users</h3>
-          <p>2000</p>
+          <div>
+            <h3>Active Users</h3>
+            <p>2000</p>
+          </div>
+          <div className="cd-stats-item-icon">
+            <BiTime />
+          </div>
         </div>
       </div>
       <CustomerDetailsList customers={customerDetails} />
