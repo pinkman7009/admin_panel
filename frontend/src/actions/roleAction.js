01dab59ba186f3f08b37d01f1a5f193d33b47661
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-11-18 11:29:39 +0530

    delete role added

--- frontend/src/actions/roleAction.js
+++ frontend/src/actions/roleAction.js
@@ -3,11 +3,21 @@ import axios from "axios";
 
 export const getUsers = () => async (dispatch) => {
   try {
-    const res = await axios.get("/api/users");
+    const res = await axios.get("/api/manageaccess");
 
     dispatch({ type: GET_ROLES, payload: res.data });
 
     console.log({ res });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const deleteUser = (id) => async (dispatch) => {
+  try {
+    console.log(id);
+    const res = await axios.delete(`/api/users/${id}`);
+    dispatch(getUsers());
   } catch (err) {
     console.error(err);
   }
