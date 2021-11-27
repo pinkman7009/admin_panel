5c8145987a3d5bad31d11891747b862a56a5cda2
vivekonfire <vvkm134@gmail.com>
2021-11-28 01:35:31 +0530

    user block and the membership plan done

--- frontend/src/actions/roleAction.js
+++ frontend/src/actions/roleAction.js
@@ -2,23 +2,32 @@ import { GET_ROLES } from "../types/roleTypes";
 import axios from "axios";
 
 export const getUsers = () => async (dispatch) => {
-  try {
-    const res = await axios.get("/api/manageaccess");
+    try {
+        const res = await axios.get("/api/manageaccess");
 
-    dispatch({ type: GET_ROLES, payload: res.data });
+        dispatch({ type: GET_ROLES, payload: res.data });
 
-    console.log({ res });
-  } catch (err) {
-    console.error(err);
-  }
+        console.log({ res });
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+export const blockUser = (id) => async (dispatch) => {
+    try {
+        const res = await axios.put(`/api/users/block/${id}`);
+        dispatch(getUsers());
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 export const deleteUser = (id) => async (dispatch) => {
-  try {
-    console.log(id);
-    const res = await axios.delete(`/api/users/${id}`);
-    dispatch(getUsers());
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        console.log(id);
+        const res = await axios.delete(`/api/users/${id}`);
+        dispatch(getUsers());
+    } catch (err) {
+        console.error(err);
+    }
 };
--- frontend/src/components/ui/customerdetails/CustomerDetailsList.js
+++ frontend/src/components/ui/customerdetails/CustomerDetailsList.js
@@ -1,43 +1,65 @@
 import React from "react";
 import ViewButton from "../buttons/ViewButton";
 import DeleteButton from "../buttons/DeleteButton";
+import { useDispatch } from "react-redux";
+import { blockUser } from "../../../actions/roleAction";
 
 const CustomerDetailsList = ({ customers }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>First Name</th>
-          <th>Last Name</th>
-          <th>Email</th>
-          <th>Phone</th>
-          <th>Options</th>
-        </tr>
-      </thead>
+    const dispatch = useDispatch();
 
-      <tbody>
-        {customers.map((item, index) => {
-          return (
-            <tr>
-              <td>{index + 1}</td>
-              <td>{item.firstname}</td>
-              <td>{item.lastname}</td>
-              <td>{item.email}</td>
-              <td>{item.phone}</td>
-              <td>
-                <div className="button-group">
-                  <ViewButton text="Edit" />
-                  <DeleteButton text="Delete" />
-                  <DeleteButton text="Block" />
-                </div>
-              </td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+    const BlockClick = (id) => {
+        dispatch(blockUser(id));
+    };
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Options</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                {customers.map((item, index) => {
+                    return (
+                        <tr>
+                            <td>{index + 1}</td>
+                            <td>{item.firstname}</td>
+                            <td>{item.lastname}</td>
+                            <td>{item.email}</td>
+                            <td>{item.phone}</td>
+                            <td>
+                                <div className="button-group">
+                                    <ViewButton text="Edit" />
+                                    <DeleteButton text="Delete" />
+                                    {item.blockedStatus ? (
+                                        <DeleteButton
+                                            text="UnBlock"
+                                            handleClick={() =>
+                                                BlockClick(item._id)
+                                            }
+                                        />
+                                    ) : (
+                                        <DeleteButton
+                                            text="Block"
+                                            handleClick={() =>
+                                                BlockClick(item._id)
+                                            }
+                                        />
+                                    )}
+                                </div>
+                            </td>
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 };
 
 export default CustomerDetailsList;
