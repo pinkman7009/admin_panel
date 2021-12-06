6a1cff26ebe839b65746607186c1c5b10e78d9ff
vivekonfire <vvkm134@gmail.com>
2021-12-06 23:54:08 +0530

    fixed bugs

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -7,64 +7,69 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 route.get("/", auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select("-password");
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send("server error");
-    }
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
+  }
 });
 
 route.post(
-    "/",
-    [body("email").isEmail(), body("password").exists()],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-        const { email, password } = req.body;
-
-        try {
-            let user = await User.findOne({ email });
-
-            if (!user) {
-                return res.status(400).json({ msg: "Invalid Credentials" });
-            }
-            const isMatch = await bcrypt.compare(password, user.password);
-
-            if (!isMatch) {
-                return res.status(400).json({ msg: "Wrong Password" });
-            }
-
-            if (user.blockedStatus) {
-                return res.status(400).json({ msg: "User Blocked" });
-            }
-
-            const payload = {
-                user: {
-                    id: user.id,
-                },
-            };
-
-            jwt.sign(
-                payload,
-                process.env.jwtSecret,
-                {
-                    expiresIn: 360000,
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({ token });
-                }
-            );
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).json({ msg: err.message });
-        }
+  "/",
+  [body("email").isEmail(), body("password").exists()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Wrong Password" });
+      }
+
+      if (user.blockedStatus) {
+        return res.status(400).json({ msg: "User Blocked" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      var ip =
+        req.headers["x-forwarded-for"] || req.socket.remoteAddress || null;
+
+      console.log(ip);
+
+      jwt.sign(
+        payload,
+        process.env.jwtSecret,
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: err.message });
+    }
+  }
 );
 
 module.exports = route;
